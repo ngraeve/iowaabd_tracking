ecb13fc6713991f531a549e448b7d4cf32e6ca00
Nick Graeve <gravy.nick@gmail.com>
2024-09-05 17:52:30 -0500

    added new log group with retention set

--- infra/iowaabd_tracking_stack.py
+++ infra/iowaabd_tracking_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
     # Duration,
     Stack,
+   aws_logs as _logs,
     aws_lambda as _lambda,
+
 )
 from constructs import Construct
 
@@ -11,10 +13,17 @@ class IowaabdTrackingStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        hello_world_function = _lambda.Function(
+        lottery_log_group = _logs.LogGroup(
+            self,
+            "Lottery Log Group",
+            retention=_logs.RetentionDays.ONE_DAY,
+            )
+
+        lottery_function = _lambda.Function(
             self,
             "HelloWorldFunction",
             runtime=_lambda.Runtime.PYTHON_3_12,
             code=_lambda.Code.from_asset("src/lambda"),
             handler="lottery.handler",
+            log_group=lottery_log_group,
         )
