05f116df2a382f92806620ddf86cdb79ed12d916
Nick Graeve <gravy.nick@gmail.com>
2024-09-06 08:54:23 -0500

    added parameter store entry + lambda timeout and env variable

--- infra/iowaabd_tracking_stack.py
+++ infra/iowaabd_tracking_stack.py
@@ -1,9 +1,9 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-   aws_logs as _logs,
+    aws_logs as _logs,
     aws_lambda as _lambda,
-
+    aws_ssm as _ssm,
 )
 from constructs import Construct
 
@@ -12,6 +12,14 @@ class IowaabdTrackingStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        lottery_parameter = _ssm.StringParameter(
+            self,
+            "LotteryParameter",
+            parameter_name='/lottery/current',
+            string_value='{}',
+            description='Contains current list of lottery liquor from Iowaabd',
+        )
 
         lottery_log_group = _logs.LogGroup(
             self,
@@ -26,4 +34,9 @@ class IowaabdTrackingStack(Stack):
             code=_lambda.Code.from_asset("src/lambda"),
             handler="lottery.handler",
             log_group=lottery_log_group,
+            timeout=Duration.seconds(10),
+            environment={
+                'lottery_parameter_name': lottery_parameter.parameter_name
+            },
+
         )
